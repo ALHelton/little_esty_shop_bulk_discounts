6c7567550c30b15abd69531bb2e44ca860bb2729
Andra Helton <andrahelton@gmail.com>
2023-03-04 14:03:04 -0700

    Add addition to edit form test for more robust testing

--- spec/features/bulk_discounts/edit_spec.rb
+++ spec/features/bulk_discounts/edit_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe 'Bulk Discount Show Page', type: :feature do
   before :each do
     @merchant1 = Merchant.create!(name: 'Hair Care')
     @fifteen = @merchant1.bulk_discounts.create!(percentage_discount: 0.15, quantity_threshold: 15)
+    @twenty = @merchant1.bulk_discounts.create!(percentage_discount: 0.20, quantity_threshold: 20)
+
     visit "/merchant/#{@merchant1.id}/bulk_discounts/#{@fifteen.id}/edit"
   end
 
@@ -12,5 +14,9 @@ RSpec.describe 'Bulk Discount Show Page', type: :feature do
     expect(page).to have_field("Percentage Discount", with: "0.15")
     expect(page).to have_field("Quantity Threshold", with: "15")
     expect(page).to have_button("Update")
+
+    expect(page).to_not have_field("Percentage Discount", with: "0.25")
+    expect(page).to_not have_field("Quantity Threshold", with: "20")
+
   end
 end
