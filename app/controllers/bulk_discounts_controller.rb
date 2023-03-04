eb5c463f0ca897c45e1c8634c5c5bb58abe95f75
Andra Helton <andrahelton@gmail.com>
2023-03-03 20:09:23 -0700

    Add functionality for delete discount button

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -23,6 +23,14 @@ class BulkDiscountsController < ApplicationController
     end
   end
 
+  def destroy
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+    @bulk_discount.destroy
+    redirect_to "/merchant/#{@merchant.id}/bulk_discounts"
+    flash[:success] = "Discount Deleted Successfully"
+  end
+
 
   private
 
