486ee67b9c6943f0e40687b1dc29e5e580e078f3
Andra Helton <andrahelton@gmail.com>
2023-03-07 12:20:29 -0700

    Add user story 7

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -15,4 +15,15 @@ RSpec.describe BulkDiscount, type: :model do
     it { should validate_numericality_of :percentage_discount }
     it { should validate_numericality_of :quantity_threshold }
   end
+
+  describe 'instance methods' do
+    before do
+      @m1 = Merchant.create!(name: 'Merchant 1')
+      @fiveoff = @m1.bulk_discounts.create!(percentage_discount: 0.05, quantity_threshold: 5)
+    end
+
+    it 'formatted_percentage' do
+      expect(@fiveoff.formatted_percentage).to eq('5%')
+    end
+  end
 end
