963ec6f5072035a344fcb22676cb05987f39261c
Andra Helton <andrahelton@gmail.com>
2023-03-07 09:35:41 -0700

    Add completed user story 6

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -27,25 +27,25 @@ class Invoice < ApplicationRecord
   #   .sum('invoice_items.quantity * invoice_items.unit_price * discount')
   # end
 
-  # def discounted_items
-  #   dis = invoice_items.joins(:bulk_discounts)
-  #   .where('invoice_items.quantity >= bulk_discounts.quantity_threshold')
-  #   .select('invoice_items.*, MAX(bulk_discounts.percentage_discount) as discount')
-  #   .group(:id)
-  # end
-
-  # def discount_total
-  #   discounted_items.sum do |di|
-  #     di.quantity * di.unit_price * di.discount
-  #   end
-  # end
-
-  def revenue_total_with_discount
-    total_discount = invoice_items.joins(:bulk_discounts)
-    .select('invoice_items.*, MAX((invoice_items.quantity * invoice_items.unit_price) * bulk_discounts.percentage_discount) AS total_discount')
+  def discounted_items
+    invoice_items.joins(:bulk_discounts)
     .where('invoice_items.quantity >= bulk_discounts.quantity_threshold')
+    .select('invoice_items.*, MAX(bulk_discounts.percentage_discount) as discount')
     .group(:id)
-    .sum(&:total_discount) 
-    total_revenue - total_discount
   end
+
+  def discount_total
+    discounted_items.sum do |di|
+      di.quantity * di.unit_price * di.discount
+    end
+  end
+
+  # def revenue_total_with_discount
+  #   total_discount = invoice_items.joins(:bulk_discounts)
+  #   .select('invoice_items.*, MAX((invoice_items.quantity * invoice_items.unit_price) * bulk_discounts.percentage_discount) AS total_discount')
+  #   .where('invoice_items.quantity >= bulk_discounts.quantity_threshold')
+  #   .group(:id)
+  #   .sum(&:total_discount) 
+  #   total_revenue - total_discount
+  # end
 end
